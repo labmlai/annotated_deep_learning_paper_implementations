06e68d012c3e93a6a6fb64eaabfa0144317cec87
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-09-27 16:40:22 +0530

    🐛 positional encoding buffer

--- labml_nn/transformers/positional_encoding.py
+++ labml_nn/transformers/positional_encoding.py
@@ -28,7 +28,7 @@ class PositionalEncoding(Module):
         super().__init__()
         self.dropout = nn.Dropout(dropout_prob)
 
-        self.register_buffer('positional_encodings', get_positional_encoding(d_model, max_len))
+        self.register_buffer('positional_encodings', get_positional_encoding(d_model, max_len), False)
 
     def __call__(self, x: torch.Tensor):
         pe = self.positional_encodings[:x.shape[0]].detach().requires_grad_(False)
