dc48f0a4e17854207c86696cb438199b66e23744
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-12-06 08:14:56 +0530

    notes

--- labml_nn/optimizers/__init__.py
+++ labml_nn/optimizers/__init__.py
@@ -1,10 +1,50 @@
 """
 # Optimizers
 
-* [Adam](adam.html)
+## Optimizer Implementations
+* [Adam Optimizer](adam.html)
+
+This [MNIST example](mnist_experiment.html) uses these optimizers.
+
+## Generic Adaptive Optimizer Base class and Weight Decay
+This file defines a common base class for *Adam* and extensions of it.
+The base class helps use implement other optimizers with minimal code
+because of re-usability.
+
+We also define a special class for L2 weight decay, so that we don't
+have to implement it inside each of the optimizers,
+and can easily extend to other weight decays like L1 without
+changing the optimizers.
+
+Here are some concepts on PyTorch optimizers:
+
+### Parameter groups
+PyTorch optimizers group parameters into sets called groups.
+Each group can have it's own hyper-parameters like learning rates.
+
+In most common cases there will be only one group.
+This is when you initialize your optimizer with,
+```python
+Optimizer(model.parameters())
+```
+
+You can define multiple parameter groups when initializing the optimizer:
+```python
+Optimizer([{'params': model1.parameters()}, {'params': model2.parameters(), 'lr': 2}])
+```
+Here we pass a list of groups. Each group is a dictionary with it's parameters under the key 'params'.
+You specify any hyper-parameters as well. If the hyper parameters are not defined they will default
+to the optimizer level defaults.
+
+You can access (and even change) these groups, and their hyper-parameters with `optimizer.param_groups`.
+Most learning rate schedule implementations I've come across do access this and change 'lr'.
+
+### States
+Optimizer maintains states (a dictionary) for each parameter (a tensor), in a dictionary `optimizer.state`.
+This is where the optimizer maintains things like exponential averages.
 """
 
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Any
 
 import torch
 from torch import nn
@@ -12,68 +52,148 @@ from torch.optim.optimizer import Optimizer
 
 
 class GenericAdaptiveOptimizer(Optimizer):
-    def __init__(self, params, defaults, lr: float, betas: Tuple[float, float], eps: float):
+    """
+    ## Base class for *Adam* and extensions
+    """
+
+    def __init__(self, params, defaults: Dict[str, Any], lr: float, betas: Tuple[float, float], eps: float):
+        """
+        ### Initialize
+
+        * `params` is the collection of parameters or set of parameter groups.
+        * `defaults` a dictionary of default hyper-parameters
+        * 'lr` is the learning rate, $\alpha$
+        * `betas` is the tuple $(\beta_1, \beta_2)$
+        * `eps` is $\epsilon$
+        """
+
+        # Check the hyper-parameters
         if not 0.0 <= lr:
-            raise ValueError("Invalid learning rate: {}".format(lr))
+            raise ValueError(f"Invalid learning rate: {lr}")
         if not 0.0 <= eps:
-            raise ValueError("Invalid epsilon value: {}".format(eps))
+            raise ValueError(f"Invalid epsilon value: {eps}")
         if not 0.0 <= betas[0] < 1.0:
-            raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
+            raise ValueError(f"Invalid beta parameter at index 0: {betas[0]}")
         if not 0.0 <= betas[1] < 1.0:
-            raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
+            raise ValueError(f"Invalid beta parameter at index 1: {betas[1]}")
+
+        # Add the hyper-parameters to the defaults
         defaults.update(dict(lr=lr, betas=betas, eps=eps))
+        # Initialize the PyTorch optimizer.
+        # This will create parameter groups with the default hyper-parameters
         super().__init__(params, defaults)
 
     def init_state(self, state: Dict[str, any], group: Dict[str, any], param: nn.Parameter):
+        """
+        ### Initialize state for a given parameter tensor
+
+        This should be overridden with code to initialize `state` for parameters `param`.
+        `group` is the parameter group dictionary to which `param` belongs.
+        """
         pass
 
     def step_param(self, state: Dict[str, any], group: Dict[str, any], grad: torch.Tensor, param: torch.Tensor):
+        """
+        ### Take optimizer step on a parameter tensor
+
+        This should be overridden and take the optimization step on `param` tensor $\theta$,
+        where `grad` is the gradient for that parameter, $g_t$,
+        `state` is the optimizer state dictionary for that parameter, and
+        `group` is the parameter group dictionary `param` belongs to.
+        """
         pass
 
     @torch.no_grad()
     def step(self, closure=None):
+        """
+        ### Optimizer step
+
+        We have created a template method that does the common stuff every *Adam* based optimizer needs.
+        """
+        # Calculate loss.
+        #
+        # 🤔 I'm not sure when you need this. I guess it's if you define a function that
+        # calculates the loss, does `loss.backward` and return the loss, instead of calling
+        # it on your own you could pass it to `optimizer.step`. 🤷‍♂️
         loss = None
         if closure is not None:
             with torch.enable_grad():
                 loss = closure()
 
+        # Iterate through the parameter groups
         for group in self.param_groups:
-            for p in group['params']:
-                if p.grad is None:
+            # Iterate through the parameters in the parameter group
+            for param in group['params']:
+                # Skip if the parameter has no gradient
+                if param.grad is None:
                     continue
-                grad = p.grad.data
+                # Get the gradient tensor
+                grad = param.grad.data
+                # We don't handle sparse gradients
                 if grad.is_sparse:
-                    raise RuntimeError('AdaBelief does not support sparse gradients,'
+                    raise RuntimeError('GenericAdaptiveOptimizer does not support sparse gradients,'
                                        ' please consider SparseAdam instead')
 
-                state = self.state[p]
-                # Lazy state initialization
+                # Get the state for the parameter
+                state = self.state[param]
+
+                # Initialize the state if state is uninitialized
                 if len(state) == 0:
-                    self.init_state(state, group, p)
+                    self.init_state(state, group, param)
 
-                self.step_param(state, group, grad, p)
+                # Take the optimization step on the parameter
+                self.step_param(state, group, grad, param)
 
+        # Return the loss, calculated from closure
         return loss
 
 
 class WeightDecay:
-    def __init__(self, weight_decay: float = 0., weight_decouple=True, absolute=False):
+    """
+    ## L2 Weight decay
+    """
+
+    def __init__(self, weight_decay: float = 0., weight_decouple: bool = True, absolute: bool = False):
+        """
+        ### Initialize weight decay
+
+        * `weight_decay` is the decay coefficient
+        * `weight_decouple` is a flag indicating whether to add the weight decay to the gradient or directly
+        decay from the parameter. If added to the  gradient it will go through the normal optimizer update.
+        * `absolute` this flag indicates whether the weight decay coefficient is absolute. This is applicable
+        when the decay is performed directly on the parameter. If this is false the actual decay is
+        `weight_decay` * `learning_rate`.
+        """
+        # Check hyper-parameters
+        if not 0.0 <= weight_decay:
+            raise ValueError(f"Invalid weight_decay value: {weight_decay}")
+
         self.absolute = absolute
         self.weight_decouple = weight_decouple
         self.weight_decay = weight_decay
-        if not 0.0 <= weight_decay:
-            raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
 
     def defaults(self):
+        """
+        Return defaults for parameter groups
+        """
         return dict(weight_decay=self.weight_decay)
 
     def __call__(self, param: torch.nn.Parameter, grad: torch.Tensor, group: Dict[str, any]):
+        """
+        ### Perform weight decay and return the gradient
+        """
+
+        # If we are doing the decay on the parameter directly
         if self.weight_decouple:
-            if not self.absolute:
-                param.data.mul_(1.0 - group['lr'] * group['weight_decay'])
-            else:
+            # If the weight decay coefficient is absolute
+            if self.absolute:
                 param.data.mul_(1.0 - group['weight_decay'])
+            # Otherwise,
+            else:
+                param.data.mul_(1.0 - group['lr'] * group['weight_decay'])
+            # Return the unmodified gradient
             return grad
         else:
             if group['weight_decay'] != 0:
+                # Add the weight decay to the gradient and return the modified gradient
                 return grad.add(param.data, alpha=group['weight_decay'])
--- labml_nn/optimizers/mnist_experiment.py
+++ labml_nn/optimizers/mnist_experiment.py
@@ -1,5 +1,7 @@
+"""
+# MNIST example to test the optimizers
+"""
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.utils.data
 
 from labml import experiment, tracker
@@ -7,31 +9,38 @@ from labml.configs import option
 from labml_helpers.datasets.mnist import MNISTConfigs
 from labml_helpers.device import DeviceConfigs
 from labml_helpers.metrics.accuracy import Accuracy
-from labml_helpers.module import Module
 from labml_helpers.seed import SeedConfigs
 from labml_helpers.train_valid import TrainValidConfigs, BatchIndex, hook_model_outputs
 from labml_nn.optimizers.configs import OptimizerConfigs
 
 
-class Net(Module):
+class Model(nn.Module):
+    """
+    ## The model
+    """
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.pool1 = nn.MaxPool2d(2)
         self.conv2 = nn.Conv2d(20, 50, 5, 1)
-        self.fc1 = nn.Linear(4 * 4 * 50, 500)
+        self.pool2 = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(16 * 50, 500)
         self.fc2 = nn.Linear(500, 10)
+        self.activation = nn.ReLU()
 
-    def __call__(self, x: torch.Tensor):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4 * 4 * 50)
-        x = F.relu(self.fc1(x))
+    def forward(self, x):
+        x = self.activation(self.conv1(x))
+        x = self.pool1(x)
+        x = self.activation(self.conv2(x))
+        x = self.pool2(x)
+        x = self.activation(self.fc1(x.view(-1, 16 * 50)))
         return self.fc2(x)
 
 
 class Configs(MNISTConfigs, TrainValidConfigs):
+    """
+    ## Configurable Experiment Definition
+    """
     optimizer: torch.optim.Adam
     model: nn.Module
     set_seed = SeedConfigs()
@@ -52,37 +61,53 @@ class Configs(MNISTConfigs, TrainValidConfigs):
         self.state_modules = [self.accuracy_func]
 
     def step(self, batch: any, batch_idx: BatchIndex):
+        # Get the batch
         data, target = batch[0].to(self.device), batch[1].to(self.device)
 
+        # Add global step if we are in training mode
         if self.mode.is_train:
             tracker.add_global_step(len(data))
 
+        # Run the model and specify whether to log the activations
         with self.mode.update(is_log_activations=batch_idx.is_last):
             output = self.model(data)
 
+        # Calculate the loss
         loss = self.loss_func(output, target)
+        # Calculate the accuracy
         self.accuracy_func(output, target)
+        # Log the loss
         tracker.add("loss.", loss)
 
+        # Optimize if we are in training mode
         if self.mode.is_train:
+            # Calculate the gradients
             loss.backward()
 
+            # Take optimizer step
             self.optimizer.step()
+            # Log the parameter and gradient L2 norms once per epoch
             if batch_idx.is_last:
                 tracker.add('model', self.model)
                 tracker.add('optimizer', (self.optimizer, {'model': self.model}))
+            # Clear the gradients
             self.optimizer.zero_grad()
 
+        # Save logs
         tracker.save()
 
 
 @option(Configs.model)
 def model(c: Configs):
-    return Net().to(c.device)
+    return Model().to(c.device)
 
 
 @option(Configs.optimizer)
 def _optimizer(c: Configs):
+    """
+    Create a configurable optimizer.
+    We can change the optimizer type and hyper-parameters using configurations.
+    """
     opt_conf = OptimizerConfigs()
     opt_conf.parameters = c.model.parameters()
     return opt_conf
@@ -93,7 +118,8 @@ def main():
     conf.inner_iterations = 10
     experiment.create(name='mnist_ada_belief')
     experiment.configs(conf, {'inner_iterations': 10,
-                              'optimizer.optimizer': 'RAdam',
+                              # Specify the optimizer
+                              'optimizer.optimizer': 'Adam',
                               'optimizer.learning_rate': 1.5e-4})
     conf.set_seed.set()
     experiment.add_pytorch_models(dict(model=conf.model))
