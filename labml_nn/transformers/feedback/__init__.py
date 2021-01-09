587410616135a8b61305864cb77feb01a5b19f58
Varuna Jayasiri <vpjayasiri@gmail.com>
2021-01-09 21:06:41 +0530

    feedback rtansformer notes

--- labml_nn/transformers/feedback/__init__.py
+++ labml_nn/transformers/feedback/__init__.py
@@ -67,45 +67,42 @@ class FeedbackAttention(Module):
         """
         ### Get relative attention scores
 
-        With absolute attention
-
         \begin{align}
-        A^{abs}_{j} &= lin_q(\color{cyan}{X^q_i + P_i})^T lin_k(\color{lightgreen}{X^k_j + P_j}) \\
-                      &= \color{cyan}{Q_i^T} \color{lightgreen}{K_j} +
-                         \color{cyan}{Q_i^T} \color{lightgreen}{U_j} +
-                         \color{cyan}{V_i^T} \color{lightgreen}{K_j} +
-                         \color{cyan}{V_i^T} \color{lightgreen}{U_j}
+        A_{j} &= lin_q(\color{cyan}{X^q + P})^T lin_k(\color{lightgreen}{X^k_j + P_j}) \\
+                      &= \color{cyan}{Q^T} \color{lightgreen}{K_j} +
+                         \color{cyan}{Q^T} \color{lightgreen}{U_j} +
+                         \color{cyan}{V^T} \color{lightgreen}{K_j} +
+                         \color{cyan}{V^T} \color{lightgreen}{U_j}
         \end{align}
 
-        where $\color{cyan}{Q_i}, \color{lightgreen}{K_j}$, are linear transformations of
-         original embeddings $\color{cyan}{X^q_i}, \color{lightgreen}{X^k_j}$
-         and $\color{cyan}{V_i}, \color{lightgreen}{U_j}$ are linear transformations of
-         absolute positional encodings $\color{cyan}{P_i}, \color{lightgreen}{P_j}$.
+        where $\color{cyan}{Q}, \color{lightgreen}{K_j}$, are linear transformations of
+         original embeddings $\color{cyan}{X^q}, \color{lightgreen}{X^k_j}$
+         and $\color{cyan}{V}, \color{lightgreen}{U_j}$ are linear transformations of
+         absolute positional encodings $\color{cyan}{P}, \color{lightgreen}{P_j}$.
 
-        They reason out that the attention to a given key should be the same regardless of
-        the position of query. Hence replace $\color{cyan}{V_i^T} \color{lightgreen}{K_j}$
-        with a constant $\color{orange}{v^T} \color{lightgreen}{K_j}$.
-        🤔 May be worthwhile testing without this assumption.
-
-        For the second and third terms relative positional encodings are introduced.
-        So $\color{cyan}{Q_i^T} \color{lightgreen}{U_j}$ is
-        replaced with $\color{cyan}{Q_i^T} \color{orange}{R_{i - j}}$
-        and $\color{cyan}{V_i^T} \color{lightgreen}{U_j}$ with $\color{orange}{S_{i-j}}$.
+        We replace $\color{cyan}{V^T} \color{lightgreen}{U_j}$ with
+        $S_j$.
 
         \begin{align}
-        A^{rel}_{i,j} &= \color{cyan}{Q_i^T} \color{lightgreen}{K_j} +
-                         \color{cyan}{Q_i^T} \color{orange}{R_{i - j}} +
-                         \color{orange}{v^T} \color{lightgreen}{K_j} +
-                         \color{orange}{S_{i-j}}
+        A^{rel}_{j} &= \underset{\mathbf{A}}{\color{cyan}{Q^T} \color{lightgreen}{K_j}} +
+                       \underset{\mathbf{B}}{\color{cyan}{Q^T} \color{lightgreen}{U_j}} +
+                       \underset{\mathbf{C}}{\color{cyan}{V^T} \color{lightgreen}{K_j}} +
+                       \underset{\mathbf{D}}{\color{orange}{S_j}}
         \end{align}
         """
 
-        # $\color{orange}{R_{i - j}}$
+        # $\color{lightgreen}{U_j}$
         key_pos_emb = self.key_pos_embeddings[-key.shape[0]:]
+        # $\color{orange}{S_j}$
         key_pos_bias = self.key_pos_bias[-key.shape[0]:]
+        # $\color{cyan}{V^T}$
         query_pos_bias = self.query_pos_bias[None, :, :]
 
+        # $\underset{\mathbf{A}}{\color{cyan}{Q^T} \color{lightgreen}{K_j}} +
+        # \underset{\mathbf{C}}{\color{cyan}{V^T} \color{lightgreen}{K_j}}$
         ac = torch.einsum('bhd,jbhd->jbh', query + query_pos_bias, key)
+        # $\underset{\mathbf{B}}{\color{cyan}{Q^T} \color{lightgreen}{U_j}} +
+        # \underset{\mathbf{D}}{\color{orange}{S_j}}$
         bd = torch.einsum('bhd,jhd->jbh', query, key_pos_emb) + key_pos_bias[:, None, :]
 
         return ac + bd
@@ -114,36 +111,36 @@ class FeedbackAttention(Module):
                  query: torch.Tensor,
                  key: torch.Tensor,
                  value: torch.Tensor):
-        # `query`, `key` and `value`  have shape `[seq_len, batch_size, d_model]`
-        batch_size, _ = query.shape
+        """
+        * `query` has shape `[batch_size, d_model]`
+        * `key` and `value` has shape `[seq_len, batch_size, d_model]`
+        """
 
         # Prepare `query`, `key` and `value` for attention computation
-        # These will then have shape `[seq_len, batch_size, heads, d_k]`
+        # `key` and `value`  will then have shape `[seq_len, batch_size, heads, d_k]`
+        # and `query` will have shape `[batch_size, heads, d_k]`
         query = self.query(query)
         key = self.key(key)
         value = self.value(value)
 
-        # Compute attention scores $Q K^T$
-        # Results in a tensor of shape `[seq_len, seq_len, batch_size, heads]`
+        # Compute attention scores
+        # Results in a tensor of shape `[seq_len, batch_size, heads]`
         scores = self.get_scores(query, key)
 
-        # Scale scores $\frac{Q K^T}{\sqrt{d_k}}$
+        # Scale scores $\frac{1}{\sqrt{d_k}}$
         scores *= self.scale
 
+        # Softmax
         attn = self.softmax(scores)
 
         # Apply dropout
         attn = self.dropout(attn)
 
-        # Multiply by values
-        # $$\underset{seq}{softmax}\Bigg(\frac{Q K^T}{\sqrt{d_k}}\Bigg)V$$
+        # Multiply by the values
         x = torch.einsum("jbh,jbhd->bhd", attn, value)
 
-        # Save attentions for any other calculations
-        self.attn = attn.detach()
-
         # Concatenate multiple heads
-        x = x.reshape(batch_size, -1)
+        x = x.reshape(x.shape[0], -1)
 
         # Output layer
         return self.output(x)
@@ -166,9 +163,9 @@ class FeedbackTransformerLayer(Module):
     def __call__(self, *,
                  x: torch.Tensor,
                  mem: Optional[torch.Tensor]):
-        # Normalize the vectors before doing self attention
-        z = self.norm_self_attn(x)
         if mem is not None:
+            # Normalize the vectors before doing self attention
+            z = self.norm_self_attn(x)
             # Run through self attention, i.e. keys and values are from self
             self_attn = self.attn(query=z, key=mem, value=mem)
             # Add the self attention results
@@ -193,27 +190,37 @@ class FeedbackTransformer(Module):
         super().__init__()
         # Make copies of the transformer layer
         self.layers = clone_module_list(layer, n_layers)
+        # Final normalization layer
         self.norm = nn.LayerNorm([layer.size])
+        #
         self.weights = nn.Parameter(torch.ones(n_layers + 1), requires_grad=True)
+        #
         self.softmax = nn.Softmax(0)
 
     def __call__(self, x_seq: torch.Tensor):
-        # Run through each transformer layer
         x_seq = torch.unbind(x_seq, dim=0)
         res = []
         mem = []
+        # For each input step
         for x in x_seq:
+            # List of embeddings from each layer
             emb = [x]
-            mem_tensor = None
-            if mem:
-                mem_tensor = torch.stack(mem)
+
+            mem_tensor = torch.stack(mem) if mem else None
+
+            # Run through each layer
             for layer in self.layers:
                 x = layer(x=x, mem=mem_tensor)
                 emb.append(x)
+
+            # Stack embeddings
             emb = torch.stack(emb)
+            # Weighted sum of embeddings
             mem.append(torch.einsum('lbd,l->bd', emb, self.softmax(self.weights)))
-            # Finally, normalize the vectors
+            # Append the output to results
             res.append(x)
 
+        # Stack the output tensors
         res = torch.stack(res)
+        # Normalize
         return self.norm(res)
