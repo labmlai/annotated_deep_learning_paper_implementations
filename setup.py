7f2e4dff07f980a80e8e995d7c4735c32e0015d9
Varuna Jayasiri <vpjayasiri@gmail.com>
2021-02-16 15:59:07 +0530

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as f:
 
 setuptools.setup(
     name='labml-nn',
-    version='0.4.87',
+    version='0.4.88',
     author="Varuna Jayasiri, Nipun Wijerathne",
     author_email="vpjayasiri@gmail.com, hnipun@gmail.com",
     description="A collection of PyTorch implementations of neural network architectures and layers.",
@@ -20,8 +20,8 @@ setuptools.setup(
                                                'labml_helpers', 'labml_helpers.*',
                                                'test',
                                                'test.*')),
-    install_requires=['labml>=0.4.97',
-                      'labml-helpers>=0.4.74',
+    install_requires=['labml>=0.4.103',
+                      'labml-helpers>=0.4.75',
                       'torch',
                       'einops',
                       'numpy'],
