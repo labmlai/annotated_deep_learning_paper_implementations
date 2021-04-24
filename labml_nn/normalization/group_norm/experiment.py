86f843eac141545e36a330de098ae0437a8154b9
Varuna Jayasiri <vpjayasiri@gmail.com>
2021-04-24 14:51:13 +0530

    📚 group norm experiment

--- labml_nn/normalization/group_norm/experiment.py
+++ labml_nn/normalization/group_norm/experiment.py
@@ -19,28 +19,42 @@ from labml_nn.normalization.group_norm import GroupNorm
 
 
 class Model(Module):
+    """
+    ### VGG model for CIFAR-10 classification
+    """
+
     def __init__(self, groups: int = 32):
         super().__init__()
         layers = []
+        # RGB channels
         in_channels = 3
+        # Number of channels in each layer in each block
         for block in [[64, 64], [128, 128], [256, 256, 256], [512, 512, 512], [512, 512, 512]]:
+            # Convolution, Normalization and Activation layers
             for channels in block:
                 layers += [nn.Conv2d(in_channels, channels, kernel_size=3, padding=1),
                            GroupNorm(groups, channels),
                            nn.ReLU(inplace=True)]
                 in_channels = channels
+            # Max pooling at end of each block
             layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+
+        # Create a sequential model with the layers
         self.layers = nn.Sequential(*layers)
+        # Final logits layer
         self.fc = nn.Linear(512, 10)
 
     def __call__(self, x):
+        # The VGG layers
         x = self.layers(x)
+        # Reshape for classification layer
         x = x.view(x.shape[0], -1)
+        # Final linear layer
         return self.fc(x)
 
 
 class Configs(CIFAR10Configs):
+    # Number of groups
     groups: int = 16
 
 
--- labml_nn/normalization/instance_norm/experiment.py
+++ labml_nn/normalization/instance_norm/experiment.py
@@ -23,24 +23,37 @@ from labml_nn.normalization.instance_norm import InstanceNorm
 
 
 class Model(Module):
+    """
+    ### VGG model for CIFAR-10 classification
+    """
+
     def __init__(self):
         super().__init__()
         layers = []
+        # RGB channels
         in_channels = 3
+        # Number of channels in each layer in each block
         for block in [[64, 64], [128, 128], [256, 256, 256], [512, 512, 512], [512, 512, 512]]:
+            # Convolution, Normalization and Activation layers
             for channels in block:
                 layers += [nn.Conv2d(in_channels, channels, kernel_size=3, padding=1),
                            InstanceNorm(channels),
                            nn.ReLU(inplace=True)]
                 in_channels = channels
+            # Max pooling at end of each block
             layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+
+        # Create a sequential model with the layers
         self.layers = nn.Sequential(*layers)
+        # Final logits layer
         self.fc = nn.Linear(512, 10)
 
     def __call__(self, x):
+        # The VGG layers
         x = self.layers(x)
+        # Reshape for classification layer
         x = x.view(x.shape[0], -1)
+        # Final linear layer
         return self.fc(x)
 
 
