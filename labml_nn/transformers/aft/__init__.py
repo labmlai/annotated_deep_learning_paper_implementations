1a586f484ab6a1ac10cd4b2ff6f7c3abf0ce45f3
Varuna Jayasiri <vpjayasiri@gmail.com>
2021-06-04 16:27:42 +0530

    docs

--- labml_nn/transformers/aft/__init__.py
+++ labml_nn/transformers/aft/__init__.py
@@ -68,21 +68,28 @@ from labml_helpers.module import Module
 
 class AFTLocal(Module):
     """
-    ### AFT Local Operation for Auto-Regression
-
-    This is an implementation of AFT Local for auto-regression, where $Y_t$
-    only has visibility to tokens before $t$:
+    ### AFT Local Operation
 
     $$Y_t = \sigma(Q_t) \odot
-     \frac{\sum_{t'=1}^t \exp(K_{t'} + w_{t,t'}) \odot V_{t'}}
-     {\sum_{t'=1}^t \exp(K_{t'} + w_{t,t'})}$$
+     \frac{\sum_{t'=1}^T \exp(K_{t'} + w_{t,t'}) \odot V_{t'}}
+     {\sum_{t'=1}^T \exp(K_{t'} + w_{t,t'})}$$
+
+    where,
+
+    \begin{align}
+    w'_{t,t'} =
+    \begin{cases}
+    w_{t,t'},  & \text{for $\lvert t-t' \rvert \lt s$} \\
+    0, & \text{otherwise}
+    \end{cases}
+    \end{align}
     """
 
     def __init__(self, d_model: int, seq_len: int, local_window_size: int, bias: bool = True):
         """
         * `d_model` is the number of features in the `query`, `key` and `value` vectors.
         * `seq_len` is $T$
-        * `s` is the local window size $s$
+        * `local_window_size` is the local window size $s$
         * `bias` is whether to have a bias parameter for transformations for $Q$, $K$ and $V$.
         """
 
@@ -96,7 +103,7 @@ class AFTLocal(Module):
         self.value = nn.Linear(d_model, d_model, bias=bias)
         # Pair-wise positional biases $w \in \mathbb{R}^{T \times T}$
         self.pos_bias = nn.Parameter(torch.zeros(seq_len, seq_len), requires_grad=True)
-        # Local mask
+        # Mask for $w_{t,t'}$
         self.local_mask = nn.Parameter(self.create_local_mask(seq_len, local_window_size), requires_grad=False)
         # Activation $\sigma$
         self.activation = nn.Sigmoid()
@@ -105,10 +112,27 @@ class AFTLocal(Module):
 
     @staticmethod
     def create_local_mask(seq_len, local_window_size):
+        """
+        #### Create local mask
+
+        This creates a mask for
+        \begin{align}
+        m_{t,t'} =
+        \begin{cases}
+        1,  & \text{for $\lvert t-t' \rvert \lt s$} \\
+        0, & \text{otherwise}
+        \end{cases}
+        \end{align}
+        """
+
+        # Initialize to ones
         local_mask = torch.ones(seq_len, seq_len, dtype=torch.bool)
-        local_mask = torch.tril(local_mask)
+        # Make $t' - t \ge s$ zero
+        local_mask = torch.tril(local_mask, local_mask - 1)
+        # Make $t - t' \ge s$ zero
         local_mask = torch.triu(local_mask, -(local_window_size - 1))
 
+        #
         return local_mask
 
     def forward(self, *,
@@ -121,8 +145,9 @@ class AFTLocal(Module):
         collection of token embeddings for  *query*, *key* and *value*.
         They have shape `[seq_len, batch_size, d_model]`.
 
-        `mask` should be `None`. We keep this parameter so that we can use this as an
-         drop in replacement for [MHA](../mha.html).
+        `mask` has shape `[seq_len, seq_len, batch_size]` and
+        `mask[i, j, b]` indicates whether for batch `b`,
+        query at position `i` has access to key-value at position `j`.
         """
 
         # `query`, `key` and `value`  have shape `[seq_len, batch_size, d_model]`
@@ -136,15 +161,37 @@ class AFTLocal(Module):
             assert mask.shape[1] == key.shape[0]
             assert mask.shape[2] == 1 or mask.shape[2] == query.shape[1]
 
+        # Transform query, key and value embeddings
         query = self.query(query)
         key = self.key(key)
         value = self.value(value)
 
+        # Get
+        #     \begin{align}
+        #     w'_{t,t'} =
+        #     \begin{cases}
+        #     w_{t,t'},  & \text{for $\lvert t-t' \rvert \lt s$} \\
+        #     0, & \text{otherwise}
+        #     \end{cases}
+        #     \end{align}
+        # using the mask
         pos_bias = self.pos_bias[:seq_len, :seq_len] * self.local_mask[:seq_len, :seq_len]
         pos_bias = pos_bias.unsqueeze(-1)
         pos_bias.masked_fill_(~mask, float('-inf'))
 
-        # We subtract $\max(K_{t'} + w_{t,t'})$ before calculating the exponents to stabilize
+        # \begin{align}
+        # Y_t &= \sigma(Q_t) \odot
+        # \frac{\sum_{t'=1}^T \exp(K_{t'} + w_{t,t'}) \odot V_{t'}}
+        # {\sum_{t'=1}^T \exp(K_{t'} + w_{t,t'})} \\
+        # &= \sigma(Q_t) \odot
+        #    \frac{\sum_{t'=1}^T \exp(w_{t,t'}) \odot \exp(K_{t'}) \odot V_{t'}}
+        #    {\sum_{t'=1}^T \exp(w_{t,t'}) \odot \exp(K_{t'})}
+        # \end{align}
+        #
+        # We compute $\exp(w_{t,t'})$, $\exp(K_{t'}) \odot V_{t'}$ and $\exp(K_{t'})$
+        # separately and do a matrix multiplication. We use einsum for clarity.
+
+        # We subtract $\max_{t'}(K_{t'})$ and $\max_{t'}(w_{t,t'})$ before calculating the exponents to stabilize
         # the softmax calculation.
         #
         # If $x_i$ is large $\exp(x_i)$ becomes huge and the computation of
@@ -152,39 +199,22 @@ class AFTLocal(Module):
         # Subtracting a constant before calculating the exponent from numerator and denominator will cancel out.
         # and can help stabilize the computation.
         # So we subtract $\max(x_i)$ to stabilize the computation.
-        #
-        # Here the maximum is the higher of $\max(K_{t'} + w_{t,t'})$ and $\max(K_{t'})$
         max_key = key.max(dim=0, keepdims=True)[0]
         max_pos_bias = pos_bias.max(dim=1,  keepdims=True)[0]
 
-        # \begin{align}
-        # Y_t &= \sigma(Q_t) \odot
-        #      \frac{\sum_{t'=1}^t \exp(K_{t'} + w_{t,t'}) \odot V_{t'}}
-        #      {\sum_{t'=1}^t \exp(K_{t'} + w_{t,t'})} \\
-        # &= \sigma(Q_t) \odot
-        #      \frac{\sum_{t'=1}^{t-s} \exp(K_{t'}) \odot V_{t'} + \sum_{t'=t-s+1}^t \exp(K_{t'} + w_{t,t'}) \odot V_{t'}}
-        #      {\sum_{t'=1}^{t-s} \exp(K_{t'}) + \sum_{t'=t-s+1}^t \exp(K_{t'} + w_{t,t'})} \\
-        # \end{align}
-        #
-        # since
-        # \begin{align}
-        # w'_{t,t'} =
-        # \begin{cases}
-        # w_{t,t'},  & \text{for $\lvert t-t' \rvert \lt s$} \\
-        # 0, & \text{otherwise}
-        # \end{cases}
-        # \end{align}
-        #
-
+        # $\exp \big(K_{t'}- \max_{t'}(K_{t'})\big)$
         exp_key = torch.exp(key - max_key)
+        # $\exp \big(w_{t,t'} - \max_{t'}(w_{t,t'})\big)$
         exp_pos_bias = torch.exp(pos_bias - max_pos_bias)
 
-        # The numerator part $\sum_{t'=1}^{t-s} \exp(K_{t'}) \odot V_{t'}$
-        # The denominator part $\sum_{t'=1}^{t-s} \exp(K_{t'})$
+        # The numerator part $\sum_{t'=1}^T \exp(w_{t,t'}) \odot \exp(K_{t'}) \odot V_{t'}$
         num = torch.einsum('ijb,jbd->ibd', exp_pos_bias, exp_key * value)
+        # The denominator part $\sum_{t'=1}^T \exp(w_{t,t'}) \odot \exp(K_{t'})$
         den = torch.einsum('ijb,jbd->ibd', exp_pos_bias, exp_key)
 
-        # Output $Y$
+        # Output $$Y_t = \sigma(Q_t) \odot
+        #         \frac{\sum_{t'=1}^T \exp(w_{t,t'}) \odot \exp(K_{t'}) \odot V_{t'}}
+        #         {\sum_{t'=1}^T \exp(w_{t,t'}) \odot \exp(K_{t'})}$$
         y = self.activation(query) * num / den
 
         # Output layer
@@ -192,6 +222,9 @@ class AFTLocal(Module):
 
 
 def _test_local_mask():
+    """
+    Test local mask
+    """
     from labml.logger import inspect
     inspect(AFTLocal.create_local_mask(10, 4))
 
