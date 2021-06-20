df32322f7e792a328abafea7a97e3ba2d11f81aa
Varuna Jayasiri <vpjayasiri@gmail.com>
2021-06-20 16:00:07 +0530

    docs

--- labml_nn/cfr/__init__.py
+++ labml_nn/cfr/__init__.py
@@ -79,10 +79,12 @@ $\pi^\sigma(h)$ is the probability of reaching the history $h$ with strategy pro
 $\pi^\sigma(h)_{-i}$ is the probability of reaching $h$ without player $i$'s contribution;
  i.e. player $i$ took the actions to follow $h$ with a probability of $1$.
 
-$$\pi^\sigma(I) = \sum_{h \in I} \pi^\sigma(h)$$
+$\pi^\sigma(h)_{i}$ is the probability of reaching $h$ with only player $i$'s contribution.
+That is,
+$$\pi^\sigma(h) = \pi^\sigma(h)_{i} \pi^\sigma(h)_{-i}$$
 
-for information set $I$, is the probability of reaching information set $I$ with
-strategy profile $\sigma$.
+Probability of reaching a information set $I$ is,
+$$\pi^\sigma(I) = \sum_{h \in I} \pi^\sigma(h)$$
 
 ### Utility (Pay off)
 
@@ -188,14 +190,14 @@ That is, $2\epsilon$-Nash equilibrium. You can similarly prove for games with mo
 
 So we need to minimize $R^T_i$ to get close to a Nash equilibrium.
 
+<a id="CounterfactualRegret"></a>
 ### Counterfactual regret
 
-**Counterfactual value** $\color{pink}{v_i(\sigma, I)}$ is the expected utility for player $i$ if the information $I$
-is reached multiplied by $\pi^\sigma_{-i}(I)$ the probability of reaching $I$.
-without player $i$'s contribution (if player $i$ took the actions leading to $I$ with a probability of $1$).
+**Counterfactual value** $\color{pink}{v_i(\sigma, I)}$ is the expected utility for player $i$ if
+ the information $I$ is reached.
 So this is like the expected utility if player $i$ tried to reach $I$.
 
-$$\color{pink}{v_i(\sigma, I)} = \sum_{z \in Z_I} \pi^\sigma_{-i}(z[I]) \pi^\sigma(z[I], z) u_i(z)$$
+$$\color{pink}{v_i(\sigma, I)} = \sum_{z \in Z_I} \pi^\sigma(z[I], z) u_i(z)$$
 
 where $Z_I$ is the set of terminal histories reachable from $I$ and
 $z[I]$ is the prefix of history $z$ upto the information set $I$.
@@ -489,36 +491,105 @@ class CFR:
                  create_new_history: Callable[[], History],
                  epochs: int,
                  n_players: int = 2):
+        """
+        * `create_new_history` creates a new empty history
+        * `epochs` is the number of iterations to train on $T$
+        * `n_players` is the number of players
+        """
         self.n_players = n_players
         self.epochs = epochs
         self.create_new_history = create_new_history
+        # A dictionary for $\mathcal{I}$ set of all information sets
         self.info_sets = {}
+        # Tracker for analytics
         self.tracker = InfoSetTracker()
 
+    def _get_info_set(self, h: History):
+        """
+        Returns the information set $I$ of the current player for a given history $h$
+        """
+        info_set_key = h.info_set_key()
+        if info_set_key not in self.info_sets:
+            self.info_sets[info_set_key] = h.new_info_set()
+        return self.info_sets[info_set_key]
+
     def walk_tree(self, h: History, i: Player, pi_i: float, pi_neg_i: float) -> float:
+        """
+        ### Walk Tree
+
+        This function walks the game tree.
+
+        * `h` is the current history $h$
+        * `i` is the player $i$ that we are computing regrets of
+        * [`pi_i`](#HistoryProbability) is
+         $\pi^{\sigma^t}_i(h)$
+        * [`pi_neg_i`](#HistoryProbability) is
+         $\pi^{\sigma^t}_{-i}(h)$
+
+        It returns the [counterfactual value](#CounterfactualRegret), for the history $h$
+        $$\color{pink}{v_i(\sigma, h)} = \sum_{z \in Z_h} \pi^\sigma(h, z) u_i(z)$$
+        where $Z_h$ is the set of terminal histories with prefix $h$
+
+        While walking the tee it updates the total regrets $\color{orange}{R^T_i(I, a)}$.
+        """
+
+        # If it's a terminal history $h \in Z$ return the terminal utility $u_i(h)$.
         if h.is_terminal():
             return h.terminal_utility(i)
+        # If it's a chance event $P(h) = c$ sample a and go to next step.
         elif h.is_chance():
             a = h.sample_chance()
             return self.walk_tree(h + a, i, pi_i, pi_neg_i)
 
-        info_set_key = h.info_set_key()
-        if info_set_key not in self.info_sets:
-            self.info_sets[info_set_key] = h.new_info_set()
-        I = self.info_sets[info_set_key]
+        # Get current player's information set for $h$
+        I = self._get_info_set(h)
+        # Initialize $\color{pink}{v_i(\sigma, h)}$
         v = 0
+        # $\color{pink}{v_i(\sigma^t |_{I \rightarrow a}, h)}$ for each action $a \in A(h)$
         va = {}
 
+        # Iterate through all actions
         for a in I.actions():
+            # If the current player is $i$,
             if i == h.player():
+                # \begin{align}
+                # \pi^{\sigma^t}_i(h + a) &= \pi^{\sigma^t}_i(h) \sigma^t_i(I)(a) \\
+                # \pi^{\sigma^t}_{-i}(h + a) &= \pi^{\sigma^t}_{-i}(h)
+                # \end{align}
                 va[a] = self.walk_tree(h + a, i, pi_i * I.strategy[a], pi_neg_i)
+            # Otherwise,
             else:
+                # \begin{align}
+                # \pi^{\sigma^t}_i(h + a) &= \pi^{\sigma^t}_i(h)  \\
+                # \pi^{\sigma^t}_{-i}(h + a) &= \pi^{\sigma^t}_{-i}(h) * \sigma^t_i(I)(a)
+                # \end{align}
                 va[a] = self.walk_tree(h + a, i, pi_i, pi_neg_i * I.strategy[a])
+            # $$\color{pink}{v_i(\sigma, h)} =
+            # \sum_{a \in A(I)} \sigma^t_i(I)(a)
+            # \color{pink}{v_i(\sigma^t |_{h \rightarrow a}, h)}$$
             v = v + I.strategy[a] * va[a]
 
+        # If the current player is $i$,
+        # update the cumulative strategies and total regrets
         if h.player() == i:
+            # Update cumulative strategies
+            # $$\sum_{t=1}^T \pi_i^{\sigma^t}(I)\color{lightgreen}{\sigma^t(I)(a)}$$
+            # where
+            # $$\pi_i^{\sigma^t}(I) = \sum_{h \in I} \pi_i^{\sigma^t}(h)$$
             for a in I.actions():
                 I.cumulative_strategy[a] = I.cumulative_strategy[a] + pi_i * I.strategy[a]
+            # \begin{align}
+            # \color{coral}{r^t_i(I, a)} &=
+            #  \pi^{\sigma^t}_{-i} (I) \Big(
+            #  \color{pink}{v_i(\sigma^t |_{I \rightarrow a}, I)} - \color{pink}{v_i(\sigma^t, I)}
+            #  \Big) \\
+            #  &=
+            #  \sum_{h \in I} \pi^{\sigma^t}_{-i} (h) \Big(
+            #  \color{pink}{v_i(\sigma^t |_{I \rightarrow a}, h)} - \color{pink}{v_i(\sigma^t, h)}
+            #  \Big) \\
+            # T \color{orange}{R^T_i(I, a)} &=
+            #  \sum_{t=1}^T \color{coral}{r^t_i(I, a)}
+            # \end{align}
             for a in I.actions():
                 I.regret[a] += pi_neg_i * (va[a] - v)
 
