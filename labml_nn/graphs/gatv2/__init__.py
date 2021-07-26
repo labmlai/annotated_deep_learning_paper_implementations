880dce2b343ac3041d50d62c3b1ca728824c8237
Varuna Jayasiri <vpjayasiri@gmail.com>
2021-07-26 14:00:04 +0530

    paragraphs

--- labml_nn/graphs/gatv2/__init__.py
+++ labml_nn/graphs/gatv2/__init__.py
@@ -7,16 +7,20 @@ summary: >
 # Graph Attention Networks v2 (GATv2)
 This is a [PyTorch](https://pytorch.org) implementation of the GATv2 operator from the paper
 [How Attentive are Graph Attention Networks?](https://arxiv.org/abs/2105.14491).
+
 GATv2s work on graph data.
 A graph consists of nodes and edges connecting nodes.
 For example, in Cora dataset the nodes are research papers and the edges are citations that
 connect the papers.
-The GATv2 operator fixes the static attention problem of the standard GAT: 
+
+The GATv2 operator fixes the static attention problem of the standard [GAT](../gat/index.html):
 since the linear layers in the standard GAT are applied right after each other, the ranking 
 of attended nodes is unconditioned on the query node. 
 In contrast, in GATv2, every node can attend to any other node.
+
 Here is [the training code](experiment.html) for training
 a two-layer GATv2 on Cora dataset.
+
 [![View Run](https://img.shields.io/badge/labml-experiment-brightgreen)](https://app.labml.ai/run/34b1e2f6ed6f11ebb860997901a2d1e3)
 """
 
