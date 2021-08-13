068225aa16ee9ae97dfdca163fa9055d5b9ab7a3
Varuna Jayasiri <vpjayasiri@gmail.com>
2021-08-13 16:30:58 +0530

    📇 version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as f:
 
 setuptools.setup(
     name='labml-nn',
-    version='0.4.107',
+    version='0.4.108',
     author="Varuna Jayasiri, Nipun Wijerathne",
     author_email="vpjayasiri@gmail.com, hnipun@gmail.com",
     description="🧠 Implementations/tutorials of deep learning papers with side-by-side notes; including transformers (original, xl, switch, feedback, vit), optimizers(adam, radam, adabelief), gans(dcgan, cyclegan, stylegan2), reinforcement learning (ppo, dqn), capsnet, distillation, etc.",
@@ -20,8 +20,8 @@ setuptools.setup(
                                                'labml_helpers', 'labml_helpers.*',
                                                'test',
                                                'test.*')),
-    install_requires=['labml>=0.4.110',
-                      'labml-helpers>=0.4.77',
+    install_requires=['labml>=0.4.129',
+                      'labml-helpers>=0.4.81',
                       'torch',
                       'einops',
                       'numpy'],
