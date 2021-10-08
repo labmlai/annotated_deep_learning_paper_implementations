92715d93af3a8ad7e6d2e3e59b7b38efd484522a
Varuna Jayasiri <vpjayasiri@gmail.com>
2021-10-08 17:24:39 +0530

    comments

--- labml_nn/diffusion/ddpm/__init__.py
+++ labml_nn/diffusion/ddpm/__init__.py
@@ -11,6 +11,12 @@ summary: >
 This is a [PyTorch](https://pytorch.org) implementation/tutorial of the paper
 [Denoising Diffusion Probabilistic Models](https://papers.labml.ai/paper/2006.11239).
 
+In simple terms, we get an image from data and add noise step by step.
+Then We train a model to predict that noise at each step and use the model to
+generate images.
+
+The following definitions and theory shows how this works.
+
 ## Forward Process
 
 The forward process adds noise to the data $x_0 \sim q(x_0)$, for $T$ timesteps.
@@ -25,19 +31,10 @@ where $\beta_1, \dots, \beta_T$ is the variance schedule.
 We can sample $x_t$ at any timestep $t$ with,
 
 \begin{align}
-q(x_t|x_0) &= \mathcal{N}(x_t; \sqrt{\bar\alpha_t} x_0, (1-\bar\alpha_t) \mathbf{I}) \\
+q(x_t|x_0) &= \mathcal{N} \Big(x_t; \sqrt{\bar\alpha_t} x_0, (1-\bar\alpha_t) \mathbf{I} \Big)
 \end{align}
 
-where $$\alpha_t = 1 - \beta_t \text{ and } \bar\alpha_t = \prod_{s=1}^t \alpha_s$$
-
-The forward process posterior conditioned by $x_0$ is,
-
-\begin{align}
-q(x_{t-1}|x_t, x_0) &= \mathcal{N}(x_{t-1}; \tilde\mu_t(x_t, x_0), \tilde\beta_t \mathbf{I}) \\
-\tilde\mu_t(x_t, x_0) &= \frac{\sqrt{\bar\alpha_{t-1}}\beta_t}{1 - \bar\alpha_t}x_0
-                         + \frac{\sqrt{\alpha_t}(1 - \bar\alpha_{t-1}}{1-\bar\alpha_t}x_t \\
-\tilde\beta_t &= \frac{1 - \bar\alpha_{t-1}}{a}
-\end{align}
+where $\alpha_t = 1 - \beta_t$ and $\bar\alpha_t = \prod_{s=1}^t \alpha_s$
 
 ## Reverse Process
 
@@ -45,20 +42,21 @@ The reverse process removes noise starting at $p(x_T) = \mathcal{N}(x_T; \mathbf
 for $T$ time steps.
 
 \begin{align}
-p_\theta(x_{t-1} | x_t) &= \mathcal{N}\big(x_{t-1}; \mu_\theta(x_t, t), \beta_t, \Sigma_\theta(x_t, t)\big) \\
-p_\theta(x_{0:T}) &= p_\theta(x_T) \prod_{t = 1}^{T} p_\theta(x_{t-1} | x_t) \\
-p_\theta(x_0) &= \int p_\theta(x_{0:T}) dx_{1:T}
+\color{cyan}{p_\theta}(x_{t-1} | x_t) &= \mathcal{N}\big(x_{t-1};
+ \color{cyan}{\mu_\theta}x_t, t), \color{cyan}{\Sigma_\theta}(x_t, t)\big) \\
+\color{cyan}{p_\theta}(x_{0:T}) &= \color{cyan}{p_\theta}(x_T) \prod_{t = 1}^{T} \color{cyan}{p_\theta}(x_{t-1} | x_t) \\
+\color{cyan}{p_\theta}(x_0) &= \int \color{cyan}{p_\theta}(x_{0:T}) dx_{1:T}
 \end{align}
 
-$\theta$ are the parameters we train.
+$\color{cyan}\theta$ are the parameters we train.
 
 ## Loss
 
 We optimize the ELBO (from Jenson's inequality) on the negative log likelihood.
 
 \begin{align}
-\mathbb{E}[-\log p_\theta(x_0)]
- &\le \mathbb{E}_q [ -\log \frac{p_\theta(x_{0:T})}{q(x_{1:T}|x_0)} ] \\
+\mathbb{E}[-\log \color{cyan}{p_\theta}(x_0)]
+ &\le \mathbb{E}_q [ -\log \frac{\color{cyan}{p_\theta}(x_{0:T})}{q(x_{1:T}|x_0)} ] \\
  &=L
 \end{align}
 
@@ -66,27 +64,36 @@ The loss can be rewritten as  follows.
 
 \begin{align}
 L
- &= \mathbb{E}_q [ -\log \frac{p_\theta(x_{0:T})}{q(x_{1:T}|x_0)} ] \\
- &= \mathbb{E}_q [ -\log p(x_T) - \sum_{t=1}^T \log \frac{p_\theta(x_{t-1}|x_t)}{q(x_t|x_{t-1})} ] \\
+ &= \mathbb{E}_q [ -\log \frac{\color{cyan}{p_\theta}(x_{0:T})}{q(x_{1:T}|x_0)} ] \\
+ &= \mathbb{E}_q [ -\log p(x_T) - \sum_{t=1}^T \log \frac{\color{cyan}{p_\theta}(x_{t-1}|x_t)}{q(x_t|x_{t-1})} ] \\
  &= \mathbb{E}_q [
   -\log \frac{p(x_T)}{q(x_T|x_0)}
-  -\sum_{t=2}^T \log \frac{p_\theta(x_{t-1}|x_t)}{q(x_{t-1}|x_t,x_0)}
-  -\log p_\theta(x_0|x_1)] \\
+  -\sum_{t=2}^T \log \frac{\color{cyan}{p_\theta}(x_{t-1}|x_t)}{q(x_{t-1}|x_t,x_0)}
+  -\log \color{cyan}{p_\theta}(x_0|x_1)] \\
  &= \mathbb{E}_q [
    D_{KL}(q(x_T|x_0) \Vert p(x_T))
-  +\sum_{t=2}^T D_{KL}(q(x_{t-1}|x_t,x_0) \Vert p_\theta(x_{t-1}|x_t))
-  -\log p_\theta(x_0|x_1)]
+  +\sum_{t=2}^T D_{KL}(q(x_{t-1}|x_t,x_0) \Vert \color{cyan}{p_\theta}(x_{t-1}|x_t))
+  -\log \color{cyan}{p_\theta}(x_0|x_1)]
 \end{align}
 
 $D_{KL}(q(x_T|x_0) \Vert p(x_T))$ is constant since we keep $\beta_1, \dots, \beta_T$ constant.
 
-### Computing $L_{t-1} = D_{KL}(q(x_{t-1}|x_t,x_0) \Vert p_\theta(x_{t-1}|x_t))$
+### Computing $L_{t-1} = D_{KL}(q(x_{t-1}|x_t,x_0) \Vert \color{cyan}{p_\theta}(x_{t-1}|x_t))$
 
-The paper sets $\Sigma_\theta(x_t, t) = \sigma_t^2 \mathbf{I}$ where $\sigma_t^2$ is set to constants
+The forward process posterior conditioned by $x_0$ is,
+
+\begin{align}
+q(x_{t-1}|x_t, x_0) &= \mathcal{N} \Big(x_{t-1}; \tilde\mu_t(x_t, x_0), \tilde\beta_t \mathbf{I} \Big) \\
+\tilde\mu_t(x_t, x_0) &= \frac{\sqrt{\bar\alpha_{t-1}}\beta_t}{1 - \bar\alpha_t}x_0
+                         + \frac{\sqrt{\alpha_t}(1 - \bar\alpha_{t-1}}{1-\bar\alpha_t}x_t \\
+\tilde\beta_t &= \frac{1 - \bar\alpha_{t-1}}{a}
+\end{align}
+
+The paper sets $\color{cyan}{\Sigma_\theta}(x_t, t) = \sigma_t^2 \mathbf{I}$ where $\sigma_t^2$ is set to constants
 $\beta_t$ or $\tilde\beta_t$.
 
 Then,
-$$p_\theta(x_{t-1} | x_t) = \mathcal{N}\big(x_{t-1}; \mu_\theta(x_t, t), \sigma_t^2 \mathbf{I} \big)$$
+$$\color{cyan}{p_\theta}(x_{t-1} | x_t) = \mathcal{N}\big(x_{t-1}; \color{cyan}{\mu_\theta}(x_t, t), \sigma_t^2 \mathbf{I} \big)$$
 
 For given noise $\epsilon \sim \mathcal{N}(\mathbf{0}, \mathbf{I})$ using $q(x_t|x_0)$
 
@@ -99,20 +106,23 @@ This gives,
 
 \begin{align}
 L_{t-1}
- &= D_{KL}(q(x_{t-1}|x_t,x_0) \Vert p_\theta(x_{t-1}|x_t)) \\
- &= \mathbb{E}_q \Bigg[ \frac{1}{2\sigma_t^2} \Vert \tilde\mu(x_t, x_0) - \mu_theta(x_t, t) \Vert^2 \Bigg] \\
+ &= D_{KL}(q(x_{t-1}|x_t,x_0) \Vert \color{cyan}{p_\theta}(x_{t-1}|x_t)) \\
+ &= \mathbb{E}_q \Bigg[ \frac{1}{2\sigma_t^2}
+ \Big \Vert \tilde\mu(x_t, x_0) - \color{cyan}{\mu_\theta}(x_t, t) \Big \Vert^2 \Bigg] \\
  &= \mathbb{E}_{x_0, \epsilon} \Bigg[ \frac{1}{2\sigma_t^2}
   \bigg\Vert \frac{1}{\sqrt{\alpha_t}} \Big(
   x_t(x_0, \epsilon) - \frac{\beta_t}{\sqrt{1 - \bar\alpha_t}} \epsilon
-  \Big) - \mu_\theta(x_t(x_0, \epsilon), t) \bigg\Vert^2 \Bigg] \\
+  \Big) - \color{cyan}{\mu_\theta}(x_t(x_0, \epsilon), t) \bigg\Vert^2 \Bigg] \\
 \end{align}
 
-Re-parameterizing
+Re-parameterizing with a model to predict noise
 
 \begin{align}
-\mu_\theta(x_t, t) &= \tilde\mu \bigg(x_t,
-  \frac{1}{\sqrt{\bar\alpha_t}} \Big(x_t -  \sqrt{1-\bar\alpha_t}\epsilon_\theta(x_t, t) \Big) \bigg) \\
-  &= \frac{1}{\sqrt{\bar\alpha_t}} \Big(x_t -  \frac{\beta_t}{\sqrt{1-\bar\alpha_t}}\epsilon_\theta(x_t, t) \Big)
+\color{cyan}{\mu_\theta}(x_t, t) &= \tilde\mu \bigg(x_t,
+  \frac{1}{\sqrt{\bar\alpha_t}} \Big(x_t -
+   \sqrt{1-\bar\alpha_t}\color{cyan}{\epsilon_\theta}(x_t, t) \Big) \bigg) \\
+  &= \frac{1}{\sqrt{\alpha_t}} \Big(x_t -
+  \frac{\beta_t}{\sqrt{1-\bar\alpha_t}}\color{cyan}{\epsilon_\theta}(x_t, t) \Big)
 \end{align}
 
 where $\epsilon_theta$ is a learned function that predicts $\epsilon$ given $(x_t, t)$.
@@ -123,7 +133,7 @@ This gives,
 L_{t-1}
 &= \mathbb{E}_{x_0, \epsilon} \Bigg[ \frac{\beta_t^2}{2\sigma_t^2 \alpha_t (1 - \bar\alpha_t)}
   \Big\Vert
-  \epsilon - \epsilon_\theta(\sqrt{\bar\alpha_t} x_0 + \sqrt{1-\bar\alpha_t}\epsilon, t)
+  \epsilon - \color{cyan}{\epsilon_\theta}(\sqrt{\bar\alpha_t} x_0 + \sqrt{1-\bar\alpha_t}\epsilon, t)
   \Big\Vert^2 \Bigg]
 \end{align}
 
@@ -132,16 +142,16 @@ That is, we are training to predict the noise.
 ### Simplified loss
 
 $$L_simple(\theta) = \mathbb{E}_{t,x_0, \epsilon} \Bigg[ \bigg\Vert
-\epsilon - \epsilon_\theta(\sqrt{\bar\alpha_t} x_0 + \sqrt{1-\bar\alpha_t}\epsilon, t)
+\epsilon - \color{cyan}{\epsilon_\theta}(\sqrt{\bar\alpha_t} x_0 + \sqrt{1-\bar\alpha_t}\epsilon, t)
 \bigg\Vert^2 \Bigg]$$
 
-This minimizes $-\log p_\theta(x_0|x_1)$ when $t=1$ and $L_{t-1}$ for $t\gt1$ discarding the
+This minimizes $-\log \color{cyan}{p_\theta}(x_0|x_1)$ when $t=1$ and $L_{t-1}$ for $t\gt1$ discarding the
 weighting in $L_{t-1}$. Discarding the weights $\frac{\beta_t^2}{2\sigma_t^2 \alpha_t (1 - \bar\alpha_t)}$
 increase the weight given to higher $t$ (which have higher noise levels), therefore increasing the sample quality.
 
 [![View Run](https://img.shields.io/badge/labml-experiment-brightgreen)](https://app.labml.ai/run/a44333ea251411ec8007d1a1762ed686)
 """
-
+from typing import Tuple, Optional
 
 import torch
 import torch.nn.functional as F
@@ -155,54 +165,115 @@ class DenoiseDiffusion:
     """
     ## Denoise Diffusion
     """
-    def __init__(self, eps_model: nn.Module, n_steps, device):
+
+    def __init__(self, eps_model: nn.Module, n_steps: int, device: torch.device):
         """
-        * `eps_model` is $\eps_\theta(x_t, t)$ model
+        * `eps_model` is $\color{cyan}{\epsilon_\theta}(x_t, t)$ model
         * `n_steps` is $t$
+        * `device` is the device to place constants on
         """
         super().__init__()
         self.eps_model = eps_model
 
+        # Create $\beta_1, \dots, \beta_T$ linearly increasing variance schedule
         self.beta = torch.linspace(0.0001, 0.02, n_steps).to(device)
 
+        # $\alpha_t = 1 - \beta_t$
         self.alpha = 1. - self.beta
+        # $\bar\alpha_t = \prod_{s=1}^t \alpha_s$
         self.alpha_bar = torch.cumprod(self.alpha, dim=0)
-
+        # $T$
         self.n_steps = n_steps
+        # $\sigma^2 = \beta$
+        self.sigma2 = self.beta
 
-        self.sigma = self.beta
+    def q_xt_x0(self, x0: torch.Tensor, t: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        #### Get $q(x_t|x_0)$ distribution
 
-    def q_xt_x0(self, x0, t):
+        \begin{align}
+        q(x_t|x_0) &= \mathcal{N} \Big(x_t; \sqrt{\bar\alpha_t} x_0, (1-\bar\alpha_t) \mathbf{I} \Big)
+        \end{align}
+        """
+
+        # $\sqrt{\bar\alpha_t} x_0$
         mean = gather(self.alpha_bar, t) ** 0.5 * x0
+        # $(1-\bar\alpha_t) \mathbf{I}$
         var = 1 - gather(self.alpha_bar, t)
+        #
         return mean, var
 
-    def q_sample(self, x0, t, noise=None):
-        if noise is None:
-            noise = torch.randn_like(x0)
+    def q_sample(self, x0: torch.Tensor, t: torch.Tensor, eps: Optional[torch.Tensor] = None):
+        """
+        #### Sample from $q(x_t|x_0)$
 
+        \begin{align}
+        q(x_t|x_0) &= \mathcal{N} \Big(x_t; \sqrt{\bar\alpha_t} x_0, (1-\bar\alpha_t) \mathbf{I} \Big)
+        \end{align}
+        """
+
+        # $\epsilon \sim \mathcal{N}(\mathbf{0}, \mathbf{I})$
+        if eps is None:
+            eps = torch.randn_like(x0)
+
+        # get $q(x_t|x_0)$
         mean, var = self.q_xt_x0(x0, t)
-        return mean + (var ** 0.5) * noise
+        # Sample from $q(x_t|x_0)$
+        return mean + (var ** 0.5) * eps
 
     def p_sample(self, xt: torch.Tensor, t: torch.Tensor):
+        """
+        #### Sample from $p(x_{t-1}|x_t)$
+
+        \begin{align}
+        \color{cyan}{p_\theta}(x_{t-1} | x_t) &= \mathcal{N}\big(x_{t-1};
+        \color{cyan}{\mu_\theta}(x_t, t), \sigma_t^2 \mathbf{I} \big) \\
+        \color{cyan}{\mu_\theta}(x_t, t)
+          &= \frac{1}{\sqrt{\alpha_t}} \Big(x_t -
+            \frac{\beta_t}{\sqrt{1-\bar\alpha_t}}\color{cyan}{\epsilon_\theta}(x_t, t) \Big)
+        \end{align}
+        """
+
+        # $\color{cyan}{\epsilon_\theta}(x_t, t)$
         eps_theta = self.eps_model(xt, t)
+        # $\bar\alpha_t$
         alpha_bar = gather(self.alpha_bar, t)
+        # $\alpha_t$
         alpha = gather(self.alpha, t)
+        # $\frac{\beta}{\sqrt{1-\bar\alpha_t}}$
         eps_coef = (1 - alpha) / (1 - alpha_bar) ** .5
+        # $$\frac{1}{\sqrt{\alpha_t}} \Big(x_t -
+        #      \frac{\beta_t}{\sqrt{1-\bar\alpha_t}}\color{cyan}{\epsilon_\theta}(x_t, t) \Big)$$
         mean = 1 / (alpha ** 0.5) * (xt - eps_coef * eps_theta)
-        var = gather(self.sigma, t) ** .5
+        # $\sigma^2$
+        var = gather(self.sigma2, t)
 
-        noise = torch.randn(xt.shape, device=xt.device)
-        return mean + var * noise
+        # $\epsilon \sim \mathcal{N}(\mathbf{0}, \mathbf{I})$
+        eps = torch.randn(xt.shape, device=xt.device)
+        # Sample
+        return mean + (var ** .5) * eps
 
-    def loss(self, x0, noise=None):
+    def loss(self, x0: torch.Tensor, noise: Optional[torch.Tensor] = None):
+        """
+        #### Simplified Loss
+
+        $$L_simple(\theta) = \mathbb{E}_{t,x_0, \epsilon} \Bigg[ \bigg\Vert
+        \epsilon - \color{cyan}{\epsilon_\theta}(\sqrt{\bar\alpha_t} x_0 + \sqrt{1-\bar\alpha_t}\epsilon, t)
+        \bigg\Vert^2 \Bigg]$$
+        """
+        # Get batch size
         batch_size = x0.shape[0]
+        # Get random $t$ for each sample in the batch
         t = torch.randint(0, self.n_steps, (batch_size,), device=x0.device, dtype=torch.long)
 
+        # $\epsilon \sim \mathcal{N}(\mathbf{0}, \mathbf{I})$
         if noise is None:
             noise = torch.randn_like(x0)
 
-        xt = self.q_sample(x0, t, noise=noise)
+        # Sample $x_t$ for $q(x_t|x_0)$
+        xt = self.q_sample(x0, t, eps=noise)
+        # Get $\color{cyan}{\epsilon_\theta}(\sqrt{\bar\alpha_t} x_0 + \sqrt{1-\bar\alpha_t}\epsilon, t)$
         eps_theta = self.eps_model(xt, t)
 
+        # MSE loss
         return F.mse_loss(noise, eps_theta)
