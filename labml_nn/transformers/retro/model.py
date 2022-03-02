cf938acf8d50f0412a4c27b8256e392132f78e76
Varuna Jayasiri <vpjayasiri@gmail.com>
2022-03-02 08:55:20 +0530

    attention layers

--- labml_nn/transformers/retro/model.py
+++ labml_nn/transformers/retro/model.py
@@ -1,15 +1,183 @@
+import math
+from typing import Set
+
+import torch
 from torch import nn
 
 
-class Encoder(nn.Module):
-    def __init__(self, n_vocab, d_model):
+class SelfAttention(nn.Module):
+    def __init__(self, d_model, n_heads, d_k, is_causal: bool):
         super().__init__()
 
-    def forward(self, h: torch.Tensor, ):
+        self.is_causal = is_causal
+        self.n_heads = n_heads
+        self.d_k = d_k
+        self.scale = 1 / math.sqrt(self.d_k)
+
+        self.query = nn.Linear(d_model, n_heads * d_k)
+        self.key = nn.Linear(d_model, n_heads * d_k)
+        self.value = nn.Linear(d_model, n_heads * d_k)
+
+        self.norm = nn.LayerNorm(d_model)
+
+        self.softmax = nn.Softmax(dim=-1)
+
+        self.output = nn.Linear(d_model, d_model)
+
+    def mask_attention(self, attn: torch.Tensor):
+        if not self.is_causal:
+            return attn
+
+        mask = torch.tril(attn.new_ones(attn.shape[1:2]))
+        return attn.masked_fill(mask[None, :, :, None] == 0, float('-inf'))
+
+    def forward(self, h: torch.Tensor):
+        """
+        h [batch, seq, d_model]
+        """
+
+        h_res = h
+        h = self.norm(h)
+
+        mh_shape = (*h.shape[:-1], self.n_heads, self.d_k)
+        q = self.query(h).view(mh_shape)
+        k = self.key(h).view(mh_shape)
+        v = self.value(h).view(mh_shape)
+
+        attn = torch.einsum('bihd,bjhd->bhij', q, k)
+        attn = attn * self.scale
+
+        attn = self.mask_attention(attn)
+
+        attn = self.softmax(attn)
+
+        h = torch.einsum("bhij,bjhd->bihd", attn, v)
+
+        h = h.reshape(h_res.shape)
+
+        h = self.output(h)
+
+        return h + h_res
+
+
+class CrossAttention(nn.Module):
+    def __init__(self, d_model, n_heads, d_k):
+        super().__init__()
+
+        self.n_heads = n_heads
+        self.d_k = d_k
+        self.scale = 1 / math.sqrt(self.d_k)
+
+        self.query = nn.Linear(d_model, n_heads * d_k)
+        self.key = nn.Linear(d_model, n_heads * d_k)
+        self.value = nn.Linear(d_model, n_heads * d_k)
+
+        self.norm = nn.LayerNorm(d_model)
+
+        self.softmax = nn.Softmax(dim=-1)
+
+        self.output = nn.Linear(d_model, d_model)
+
+    def forward(self, e: torch.Tensor, kv: torch.Tensor):
+        """
+        e [batch_size, chunks, neighbors, seq, d_model]
+        kv [batch_size, chunks, seq, d_model]
+        """
+
+        e_res = e
+        e = self.norm(e)
+
+        q = self.query(e).view(*e.shape[:-1], self.n_heads, self.d_k)
+        k = self.key(e).view(*kv.shape[:-1], self.n_heads, self.d_k)
+        v = self.value(e).view(*kv.shape[:-1], self.n_heads, self.d_k)
+
+        attn = torch.einsum('bcnihd,bcjhd->bcnhij', q, k)
+        attn = attn * self.scale
+
+        attn = self.softmax(attn)
+
+        e = torch.einsum("bcnhij,bcjhd->bcnihd", attn, v)
+
+        e = e.reshape(e_res.shape)
+
+        e = self.output(e)
+
+        return e + e_res
+
+
+class ChunkedCrossAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+class FeedForward(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+class Encoder(nn.Module):
+    def __init__(self, chunk_len: int, n_layers: int, ca_layers: Set[int]):
+        super().__init__()
+        self.ca_layers = ca_layers
+        self.chunk_len = chunk_len
+        self.ca = nn.ModuleList([CrossAttention() for _ in range(len(ca_layers))])
+        self.attn = nn.ModuleList([SelfAttention() for _ in range(n_layers)])
+        self.ffw = nn.ModuleList([FeedForward() for _ in range(n_layers)])
+
+    def forward(self, e: torch.Tensor, h: torch.Tensor):
+        """
+        e [batch_size, chunks, neighbors, seq, d_model]
+        h [batch_size, seq, d_model]
+        """
+
+        batch_size, chunks, neighbors, neighbor_len, d_model = e.shape
+        h_split = h[:, self.chunk_len * chunks, :].reshape(batch_size, chunks, self.chunk_len, d_model)
+
+        p_ca = 0
+        for p in range(len(self.attn)):
+            e = self.attn[p](e.view(-1, neighbor_len, d_model)).view(e.shape)
+
+            if p in self.ca_layers:
+                e = self.ca[p_ca](e, h_split)
+                p_ca += 1
+
+            e = self.ffw[p](e)
+
+        return e
+
 
 class Model(nn.Module):
-    def __init__(self, n_vocab, d_model):
+    def __init__(self, n_vocab: int, d_model: int, n_layers: int, ca_layers: Set[int], chunk_length: int):
         super().__init__()
 
+        self.ca_layers = ca_layers
         self.emb = nn.Embedding(n_vocab, d_model)
-        self.encoder = Encoder()
+        self.encoder = Encoder(chunk_length)
+        self.cca = nn.ModuleList([ChunkedCrossAttention() for _ in range(len(ca_layers))])
+        self.attn = nn.ModuleList([SelfAttention() for _ in range(n_layers)])
+        self.ffw = nn.ModuleList([FeedForward() for _ in range(n_layers)])
+        self.read = nn.Linear(d_model, n_vocab)
+
+    def forward(self, x: torch.Tensor, ret: torch.Tensor):
+        """
+        x [batch_size, seq]
+        e [batch_size, chunks, neighbors, seq]
+        """
+
+        h = self.emb(x)
+        ret_emb = self.emb(ret)
+
+        p_ca = 0
+        for p in range(len(self.attn)):
+            h = self.attn[p](h)
+
+            if p == min(self.retro_layers):
+                e = self.encoder(ret_emb, h)
+
+            if p in self.ca_layers:
+                h = self.cca[p_ca](h, e)
+                p_ca += 1
+
+            h = self.ffw[p](h)
+
+        return self.read(h)
