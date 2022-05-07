274204e58a903126f0201989ccb208e94ed3bdc3
Varuna Jayasiri <vpjayasiri@gmail.com>
2022-05-07 12:46:45 +0100

    gpt2 experiment

--- labml_nn/sampling/experiment.py
+++ labml_nn/sampling/experiment.py
@@ -1,6 +1,6 @@
 import torch
 
-from labml import monit, logger
+from labml import monit, logger, lab
 
 from labml.logger import Text
 
@@ -9,12 +9,13 @@ from labml_nn.sampling.greedy import GreedySampler
 from labml_nn.sampling.nucleus import NucleusSampler
 from labml_nn.sampling.temperature import TemperatureSampler
 from labml_nn.sampling.top_k import TopKSampler
-from labml_nn.sampling.utils import get_model_dataset
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 
-def sample(model, ds, sampler: Sampler, n_samples: int, n_tokens: int, seq_len: int, prompt: str):
+def sample(model: GPT2LMHeadModel, tokenizer: GPT2Tokenizer, sampler: Sampler,
+           n_samples: int, n_tokens: int, seq_len: int, prompt: str):
     with torch.no_grad():
-        data = torch.tile(ds.text_to_i(prompt)[:, None], (1, n_samples))
+        data = torch.tile(torch.tensor(tokenizer.encode(prompt))[None, :], (n_samples, 1))
 
         # Collect output for printing
         logs = [[(prompt, Text.meta)] for _ in range(n_samples)]
@@ -23,14 +24,14 @@ def sample(model, ds, sampler: Sampler, n_samples: int, n_tokens: int, seq_len: 
             # Tokenize the prompt
             data = data[-seq_len:]
             # Get the model output
-            logits, *_ = model(data)
-            logits = logits[-1]
+            logits = model(data)[0]
+            logits = logits[:, -1]
             # Get the model prediction (greedy)
             res = sampler(logits)
-            data = torch.cat([data, res[None, :]], dim=0)
+            data = torch.cat([data, res[:, None]], dim=1)
             # Add the prediction for logging
             for j in range(n_samples):
-                logs[j] += [('' + ds.itos[res[j]], Text.value)]
+                logs[j] += [('' + tokenizer.decode(res[j]), Text.value)]
 
     # Print the sampled output
     for j in range(n_samples):
@@ -38,26 +39,30 @@ def sample(model, ds, sampler: Sampler, n_samples: int, n_tokens: int, seq_len: 
 
 
 def main():
-    model, ds = get_model_dataset('074d4004cc6b11ecad7a0242ac1c0002')
+    with monit.section('Load tokenizer/model'):
+        tokenizer = GPT2Tokenizer.from_pretrained('gpt2', cache_dir=lab.get_data_path() / 'cache')
+        model = GPT2LMHeadModel.from_pretrained('gpt2', cache_dir=lab.get_data_path() / 'cache')
     model.eval()
 
+    prompt = 'I saw an interesting dream last night. '
+
     with monit.section('greedy'):
-        sample(model, ds, GreedySampler(), 4, 32, 128, 'It is')
-    #
-    # with monit.section('temperature=1.'):
-    #     sample(model, ds, TemperatureSampler(1.), 4, 32, 128, 'It is')
-    # with monit.section('temperature=.1'):
-    #     sample(model, ds, TemperatureSampler(.1), 4, 32, 128, 'It is')
-    # with monit.section('temperature=10.'):
-    #     sample(model, ds, TemperatureSampler(10.), 4, 32, 128, 'It is')
+        sample(model, tokenizer, GreedySampler(), 4, 32, 128, prompt)
 
-    # with monit.section('top_k=5'):
-    #     sample(model, ds, TopKSampler(2, TemperatureSampler(1.)), 4, 32, 128, 'It is')
+    with monit.section('temperature=1.'):
+        sample(model, tokenizer, TemperatureSampler(1.), 4, 32, 128, prompt)
+    with monit.section('temperature=.1'):
+        sample(model, tokenizer, TemperatureSampler(.1), 4, 32, 128, prompt)
+    with monit.section('temperature=10.'):
+        sample(model, tokenizer, TemperatureSampler(10.), 4, 32, 128, prompt)
 
-    with monit.section('nucles p=.95'):
-        sample(model, ds, NucleusSampler(0.95, TemperatureSampler(1.)), 4, 32, 128, 'It is')
-    with monit.section('nucles p=.95'):
-        sample(model, ds, NucleusSampler(0.1, TemperatureSampler(1.)), 4, 32, 128, 'It is')
+    with monit.section('top_k=5'):
+        sample(model, tokenizer, TopKSampler(2, TemperatureSampler(1.)), 4, 32, 128, prompt)
+
+    with monit.section('nucleus p=.95'):
+        sample(model, tokenizer, NucleusSampler(0.95, TemperatureSampler(1.)), 4, 32, 128, prompt)
+    with monit.section('nucleus p=.1'):
+        sample(model, tokenizer, NucleusSampler(0.1, TemperatureSampler(1.)), 4, 32, 128, prompt)
 
 
 if __name__ == '__main__':
