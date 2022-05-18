5d3348d0c3e58185b5f1d1c2773e4158e0b4f393
Varuna Jayasiri <vpjayasiri@gmail.com>
2022-05-18 10:10:59 +0530

    comet link

--- labml_nn/normalization/deep_norm/experiment.py
+++ labml_nn/normalization/deep_norm/experiment.py
@@ -9,6 +9,7 @@ summary: >
 
 [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/labmlai/annotated_deep_learning_paper_implementations/blob/master/labml_nn/normalization/deep_norm/experiment.ipynb)
 [![View Run](https://img.shields.io/badge/labml-experiment-brightgreen)](https://app.labml.ai/run/ec8e4dacb7f311ec8d1cd37d50b05c3d)
+[![Open In Comet](https://images.labml.ai/images/comet.svg?experiment=deep_norm&file=experiment)](https://www.comet.ml/labml/deep-norm/61d817f80ff143c8825fba4aacd431d4?experiment-tab=chart&showOutliers=true&smoothing=0&transformY=smoothing&xAxis=step)
 """
 
 import copy
