1c36aad2377968e761a1f70212acdc38880321b8
Varuna Jayasiri <vpjayasiri@gmail.com>
2022-08-11 15:19:25 +0530

    zero3 notes

---
+++ labml_nn/scaling/__init__.py
@@ -0,0 +1,11 @@
+"""
+---
+title: Large scale model training
+summary: >
+    Large scale model training/inference implementations.
+---
+
+# Large scale model training
+
+* [Zero-DP optimizer](zero3/index.html)
+"""
