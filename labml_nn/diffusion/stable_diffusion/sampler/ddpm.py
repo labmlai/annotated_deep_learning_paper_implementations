d4c19008384ee54d2cdb9b855f4ef46036db7a29
Varuna Jayasiri <vpjayasiri@gmail.com>
2022-11-13 11:32:16 +0530

    fix sqrt_1m_alpha_bar

--- labml_nn/diffusion/stable_diffusion/sampler/ddpm.py
+++ labml_nn/diffusion/stable_diffusion/sampler/ddpm.py
@@ -68,7 +68,7 @@ class DDPMSampler(DiffusionSampler):
             # $\sqrt{\bar\alpha}$
             self.sqrt_alpha_bar = alpha_bar ** .5
             # $\sqrt{1 - \bar\alpha}$
-            self.sqrt_1m_alpha_bar = alpha_bar ** .5
+            self.sqrt_1m_alpha_bar = (1. - alpha_bar) ** .5
             # $\frac{1}{\sqrt{\bar\alpha_t}}$
             self.sqrt_recip_alpha_bar = alpha_bar ** -.5
             # $\sqrt{\frac{1}{\bar\alpha_t} - 1}$
